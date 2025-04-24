4ae6159d6a00b0b298d407e200521465a5f06f20
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-04-23 20:48:51 -0400

    fix logos transportacion, app name modelos,tilde dentro de reporte Delitos en perdidas

--- apps/project/apps.py
+++ apps/project/apps.py
@@ -4,6 +4,8 @@ from django.apps import AppConfig
 class ProjectConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.project"
+    verbose_name = "Modelos"
+
 
     def ready(self):
         from . import signals  # noqa
